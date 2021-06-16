2e29e972602ed15e720fc7b3619b39b00c1cdc78
hhv1996 <hhv1996@gmail.com>
2021-06-15 21:38:46 -0300

    se agrego pagina defult

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app = express();
 const Ruta = require('./routes/RutaObtenerRuta');
 
 app.use('/api/ruta', Ruta);
-
+app.get('/api', async (req, res,next) => {
+    res.send("RecolecAR API");
+  });
 app.listen(3000, () => {
  console.log("El servidor est� inicializado en el puerto 3000");
 });
